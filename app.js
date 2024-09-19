0bdc7753944369b1f96566655fb54b748d1168bc
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2024-09-19 04:47:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 //enabling cors
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "http://hourglass-corp.netlify.app/",
     credentials: true,
   })
 );
