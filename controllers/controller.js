58fe0249bfed653f9c2997456c473c07614ec8ec
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2024-10-13 14:47:00 +0100

    added create account service

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,16 @@
 const sendMail = require("../services/sendmail.service");
+const createAccount = require("../services/createAccount.service");
 
-const signUpHandler = (req, res) => {
-  sendMail(req.body, res);
+const signUpHandler = async (req, res) => {
+  const accountDetails = await createAccount(req.body);
+  if (accountDetails.requestSuccessful === true) {
+    sendMail(req.body, res, accountDetails);
+  } else {
+    console.log(`Bank API response: ${accountDetails.responseMessage}`);
+    res.status(500).json({
+      message: `Error: failed to generate account number at this time. ERLOCATION: controller.js, APIRESPONSE: ${accountDetails.responseMessage}`,
+    });
+  }
 };
 
 const refreshHandler = (req, res) => {
