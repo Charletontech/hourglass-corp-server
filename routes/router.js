8a02aa91ce24b73d802986050b33dc725ecf97e7
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2024-10-21 07:50:26 +0100

    final

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { signUpHandler, refreshHandler } = require("../controllers/controller");
+const {
+  signUpHandler,
+  refreshHandler,
+  webhookHandler,
+  initDBHandler,
+} = require("../controllers/controller");
 
 // public routes
 router.post("/signup", signUpHandler);
+router.get("/initDB", initDBHandler);
 router.get("/refresh-server", refreshHandler);
+router.post("/webhook", webhookHandler);
+
 module.exports = router;
