ccd75aacf0abb09e9051b4a33b378df662a2775c
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2024-10-21 08:28:36 +0100

    Update port

--- database/main.database.js
+++ database/main.database.js
@@ -5,7 +5,7 @@ const connectDB = mysql.createConnection({
   host: `${process.env.HOURGLASS_HOST}`,
   user: `${process.env.HOURGLASS_USER}`,
   password: `${process.env.HOURGLASS_PASSWORD}`,
-  port: 3306,
+  port: 3307,
   database: `${process.env.HOURGLASS_DB}`,
 });
 
