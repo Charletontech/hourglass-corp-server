4adc418b0de0a3e56e50a7957a89638206bac39c
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2024-11-09 09:06:54 +0100

    Update main.database.js

--- database/main.database.js
+++ database/main.database.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const connectDB = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
+  host: process.env.BLOOMZON_HOST,
+  user:  process.env.BLOOMZON_USER,
+  password:  process.env.BLOOMZON_PASS,
   port: 3306,
-  database: "bloomzon",
+  database:  process.env.BLOOMZON_DB,
 });
 
 module.exports = connectDB;
