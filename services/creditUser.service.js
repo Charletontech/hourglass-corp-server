51b66686146fed389506f9602e3bab5abd2ea489
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2025-04-13 09:30:38 +0100

    corrected sql for debitting user

--- services/creditUser.service.js
+++ services/creditUser.service.js
@@ -3,7 +3,7 @@ const connectDB = require("../database/main.database");
 
 const creditUser = async ({ fundAmount, phone }) => {
   return new Promise((resolve, reject) => {
-    var sql = `UPDATE hourglass_users SET wallet = wallet + ${fundAmount} WHERE phone = ${phone}`;
+    var sql = `UPDATE hourglass_users SET wallet = wallet + ${fundAmount} WHERE phone = "${phone}"`;
     connectDB.query(sql, (err, result) => {
       if (err) {
         console.log(err);
