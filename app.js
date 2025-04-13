4d315b96e513e6653e3708bd011003b14aca942d
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2025-04-13 04:56:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ app.use(bodyParser.json());
 
 //enabling cors
 // app.use(cors({ origin: true }));
- app.use(
-   cors({
-     origin: "https://hourglassnig.online",
-     credentials: true,
-   })
- );
-// app.options("*", cors());
+ //app.use(
+  // cors({
+    // origin: "https://hourglassnig.online",
+     //credentials: true,
+   //})
+// );
+ app.options("*", cors());
 
 // initialize database
 // initDB();
