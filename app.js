ef93e4fc797e73b57ae315d8d4e46f72a84e8c5f
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2025-04-13 05:17:32 +0100

    cors again 2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // app.use(cors({ origin: true }));
  app.use(
    cors({
-     origin: "https:hourglassnig.online",
+     origin: "https://hourglassnig.online",
      credentials: true,
    })
  );
